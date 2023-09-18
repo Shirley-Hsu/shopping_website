5279728f6b0f0b81d325238f8f45dcdb7a5b7772
Shirley <shirley41825@gmail.com>
2023-09-18 22:14:46 +0800

    update mysql ip

--- link_sql.php
+++ link_sql.php
@@ -1,4 +1,4 @@
 <?php
-$link = mysqli_connect("localhost", "root", "root123456", "shop") // 建立MySQL的資料庫連結
+$link = mysqli_connect("140.115.54.86", "root", "root123456", "shop") // 建立MySQL的資料庫連結
 or die("無法開啟MySQL資料庫連結!<br>");
 ?>
